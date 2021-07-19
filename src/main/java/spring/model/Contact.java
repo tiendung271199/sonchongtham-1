8f4603f6de853a72bf1025b11961bcbac10b6544
tiendung271199 <66504622+tiendung271199@users.noreply.github.com>
2021-07-19 21:23:08 +0700

    Update Contact.java

--- src/main/java/spring/model/Contact.java
+++ src/main/java/spring/model/Contact.java
@@ -2,6 +2,9 @@ package spring.model;
 
 import java.sql.Timestamp;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,12 +16,17 @@ public class Contact {
 	
 	private int id;
 	
+	@NotBlank
 	private String name;
 	
+	@NotBlank
+	@Email
 	private String email;
 	
+	@NotBlank
 	private String phone;
 	
+	@NotBlank
 	private String content;
 	
 	private int status;
