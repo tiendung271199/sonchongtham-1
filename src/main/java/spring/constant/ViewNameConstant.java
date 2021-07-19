2c899b816c6123e3d5fedbc712a11ca9cbecc86c
tiendung271199 <66504622+tiendung271199@users.noreply.github.com>
2021-07-19 21:24:51 +0700

    Update ViewNameConstant.java

--- src/main/java/spring/constant/ViewNameConstant.java
+++ src/main/java/spring/constant/ViewNameConstant.java
@@ -3,10 +3,20 @@ package spring.constant;
 public class ViewNameConstant {
 
 	public static final String INDEX = "project.index";
+	public static final String BLOG = "project.blog";
+	public static final String CAT = "project.cat";
 	public static final String DETAIL = "project.detail";
 	public static final String CONTACT = "project.contact";
 
 	public static final String ADMIN_INDEX = "admin.index";
 	public static final String CAT_INDEX = "admin.cat.index";
+
+	public static final String CAT_ADD = "admin.cat.add";
 	public static final String PRODUCT_INDEX = "admin.product.index";
+
+
+	public static final String ERROR_404 = "views/error/404";
+	public static final String ERROR_403 = "views/error/403";
+	public static final String ERROR_400 = "views/error/400";
+
 }
