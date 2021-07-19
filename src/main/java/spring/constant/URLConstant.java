1de050e91c3ad21d2522b44d01027f682e10f392
tiendung271199 <66504622+tiendung271199@users.noreply.github.com>
2021-07-19 21:26:01 +0700

    Update URLConstant.java

--- src/main/java/spring/constant/URLConstant.java
+++ src/main/java/spring/constant/URLConstant.java
@@ -5,11 +5,23 @@ public class URLConstant {
 	public static final String URL_EMPTY = "";
 
 	public static final String URL_INDEX = "";
+	public static final String URL_BLOG = "tin-tuc";
+	public static final String URL_BLOG_PAGINATION = "tin-tuc/trang-{page}";
+	public static final String URL_CAT = "danh-muc/{name}/{id}";
+	public static final String URL_CAT_PAGINATION = "danh-muc/{name}/{id}/trang-{page}";
 	public static final String URL_CONTACT = "lien-he";
 	public static final String URL_DETAIL = "chi-tiet/{name}/{id}";
 	
 	public static final String URL_ADMIN = "admin";
 	public static final String URL_ADMIN_INDEX = "trang-chu.html";
 	public static final String URL_ADMIN_CAT_INDEX = "danh-muc.html";
+	public static final String URL_ADMIN_CAT_ADD = "danh-muc/them-danh-muc.html";
+	public static final String URL_ADMIN_CAT_EDIT = "danh-muc/sua-danh-muc-{id}.html";
 	public static final String URL_ADMIN_PRODUCT_INDEX = "san-pham.html";
+	
+	public static final String ERROR = "error";
+	public static final String ERROR_404 = "404";
+	public static final String ERROR_403 = "403";
+	public static final String ERROR_400 = "400";
+	
 }
